ae8fccee13755d5dd262179e234461dbab308c0f
Shon <shonaz900@outlook.com>
2022-06-20 13:43:59 +0300

    add full model

---
+++ FlightControl.Data/Models/Flight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightControl.Data.Models
+{
+    public class Flight
+    {
+        public int FlightId { get; set; }
+        public string Name { get; set; }
+        public DateTime? ActualArrivalTime { get; set; }
+        public DateTime ScheduledArrivalTime { get; set; }
+        public bool IsCanceled { get; set; }
+        public int TimeDelayOnGround { get; set; }
+        public int TimeDelayOnFlight { get; set; }
+        public int PlaceOnAirPort { get; set; }
+
+    }
+}
